49c101df2f33c5d57a762df81cb79e300f036429
Sattavath Phitsavath <huangguifu21@gmail.com>
2024-02-17 13:53:08 +0700

    The new .m

---
+++ backOfficeApp/Processes/pos_db.m
@@ -0,0 +1,98 @@
+{
+    "orderDate": "27/01/2001",
+    "orderStatus": {
+        "statusName": "Pending"
+    },
+    "shippingMethod": {
+        "methodName": "In-store Pickup"
+    },
+    "customerTransferPics": {
+        "picPath": "C:/Users/Sattavath/Desktop/Transaction/416472943_1754813021611319_4025405789920511094_n"
+    },
+    "discount": {
+        "Min_Spend": 4000,
+        "Start": "2023/12/03",
+        "Duration": 2,
+        "Amount": 20,
+        "Shipping_Method": {
+            "Method_Name": "In-store Pickup"
+        },
+        "Description": "new year happiness"
+    },
+    "deliveryBranch": {
+        "deliveryService": {
+            "deliveryCompanyName": "Anousith Express"
+        },
+        "branchName": "ສາຂາ ໂພນຕ້ອງສະຫວາດ(ຈັນທະບູລີ)",
+        "branchTelNumbers": [
+            {
+                "telNumber": "020-95960461"
+            }
+        ],
+        "branchAvailability": "open",
+        "branchAddress": {
+            "province": "ນະຄອນຫຼວງວຽງຈັນ",
+            "city": "ຈັນທະບູລີ",
+            "village": "ໂພນຕ້ອງສະຫວາດ",
+            "addressDetail": "ຂ້າງວົງວຽນ ຕິດກັບບໍລິສັດ SKC"
+        },
+        "branchGooglemapPosition": "?saddr=17.995622,102.6241915&daddr=17.99646520515873,102.62392149999998"
+    },
+    "staff": {
+        "staffFirstName": "andy",
+        "staffLastName": "o",
+        "staffBirthday": "19/01/2001",
+        "email": "huangguifu21@gmail.com",
+        "staffPhoneNumber": "",
+        "permission": {
+            "permissionName": "Admin"
+        }
+    },
+    "customer": {
+        "customerFirstName": "andy",
+        "customerLastName": "o",
+        "customerBirthday": "19/01/2001",
+        "customerPhoneNumber": "",
+        "customerSocialLink": "",
+        "gender": "",
+        "email": "huangguifu21@gmail.com"
+    },
+    "billItems": [
+        {
+            "shoeSize": {
+                "sizeType": "EU",
+                "sizeNumber": 38.5
+            },
+            "productCondition": {
+                "conditionName": "Brand New"
+            },
+            "product": {
+                "productName": "Jordan 4 Retro Thunder (2023)",
+                "retailPrice": 5000,
+                "sellingPrice": 5200,
+                "SKU": "DH6927-017",
+                "colorway": "BLACK/TOUR YELLOW",
+                "releaseDate": "13/05/2023",
+                "amountSold": 1494,
+                "productWithSizes": [
+                    {
+                        "inventoryQty": 20,
+                        "shoeSize": {
+                            "sizeType": "EU",
+                            "sizeNumber": 38.5
+                        }
+                    }
+                ],
+                "productCollection": {
+                    "collectionName": "Jordan 1",
+                    "brand": {
+                        "brandName": "Jordan"
+                    }
+                },
+                "productImage": {
+                  "productImagePath":""
+                }
+            }
+        }
+    ]
+}
